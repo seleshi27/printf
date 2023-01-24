002c69d39c37d66f68d285c4c03c2f89677c9916
LizekaD <118312998+LizekaD@users.noreply.github.com>
2023-01-24 23:13:38 +0200

    Update handle_print.c

--- handle_print.c
+++ handle_print.c
@@ -2,7 +2,7 @@
 /**
  * handle_print - Prints an argument based on its type
  * @fmt: Formatted string in which to print the arguments.
- * @list: List of arguments to be printed.
+ * @list: List of arguments to be printed
  * @ind: ind.
  * @buffer: Buffer array to handle print.
  * @flags: Calculates active flags
